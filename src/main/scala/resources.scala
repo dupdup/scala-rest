c01a8c296b19b1e3e9d14b9dbd38a38dd963a652
Felipe Oliveira <felipera@Felipe-Oliveiras-MacBook-Pro.local>
2011-02-13 18:42:15 -0500

    adding jackson for json serialize/deserialize

--- src/main/scala/resources.scala
+++ src/main/scala/resources.scala
@@ -6,23 +6,26 @@ import java.util._
 import java.lang.{Long => JLong}
 import javax.persistence.Id
 import com.googlecode.objectify.annotation.Cached
-
+import scala.reflect.BeanProperty
 
 package net.activedatatech.vendor.data {
 
+	@BeanProperty
 	trait Contact {
 	
-	  var workPhone: String = _
-	  var mobilePhone: String = _
-	  var fax: String = _
-	  var email: String = _
-	  var address: String = _
+	  @BeanProperty var workPhone: String = _
+	  @BeanProperty var mobilePhone: String = _
+	  @BeanProperty var fax: String = _
+	  @BeanProperty var email: String = _
+	  @BeanProperty var address: String = _
+	  @BeanProperty var name = "someone"
 	
 	}
 
+	@BeanProperty
 	class Vendor extends AnyRef with Contact {
 	
-		@Id var id: String = _
+		@Id @BeanProperty var id: String = _
 	
 	}
 
@@ -45,12 +48,12 @@ package net.activedatatech.vendor.rest {
 	  @GET
 	  @Path("/hello.json")
 	  @Produces(Array("application/json"))
-	  def getMediaType = { new Vendor }
+	  def getJson = { new Vendor }
 	
 	  @GET
 	  @Path("/hello.xml")
 	  @Produces(Array("text/xml"))
-	  def getHTML = "hello"
+	  def getXml = { new Vendor }
 	}
 	
 
